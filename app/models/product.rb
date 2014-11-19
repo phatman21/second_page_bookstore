829a731656a7b92bd727b68d533eb00ad04bc431
Blaine D. Davidson <blaine.davidson95@gmail.com>
2014-11-18 21:46:52 -0600

    installed rubocop and cleaned offences

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   validates :name, :price, :stock_quantity, presence: true
-  validates :price, :stock_quantity, numericality: true 
+  validates :price, :stock_quantity, numericality: true
 end
