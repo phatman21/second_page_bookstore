f7cc48318ffe5c18f0f518b13f64ef02379a0f7f
Blaine D. Davidson <blaine.davidson95@gmail.com>
2014-12-09 17:07:39 -0600

    added pagination to search and category views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,14 +9,10 @@ class ProductsController < ApplicationController
     @category = Category.all
   end
 
-  def show_category
-    @product = Product.find(params[:category.name])
-  end
-
   def search_results
     # we will do a search here
     wildcard_keywords = '%' + params[:search_keywords] + '%'
-    @product = Product.where('name LIKE ?', wildcard_keywords)
+    @product = Product.where('name LIKE ?', wildcard_keywords).page(params[:page]).per(6)
     @category = Category.all
   end
   # view app/views/search_results.html.slim
